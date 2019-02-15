367068df7c2e940c1ea97f37f66ee896c52b303c
Jasper Chan <jasperchan515@gmail.com>
2019-02-15 14:24:37 -0800

    Implement carry, arbitrary length count

--- Labs/Lab 3/clock_myhdl/ring_counter.py
+++ Labs/Lab 3/clock_myhdl/ring_counter.py
@@ -1,21 +1,26 @@
 from myhdl import block, Signal, always, instance, intbv, modbv
+from math import log2, ceil
 
 
 @block
-def ring_counter(clk, rst, dout, din, ld, min_val=0, max_val=9):
-    val = Signal(modbv(min_val, min=min_val, max=2**4))
+def ring_counter(clk, rst, dout, din, ld, carry_out, min_val=0, max_val=9):
+    val = Signal(modbv(min_val, min=min_val, max=2**ceil(log2(9))))
 
     @always(clk, rst, ld)
     def logic():
+        carry_out.next = 0
         if not rst:
             val.next = min_val
         elif not ld:
-            val.next = din
+            if din > max_val:
+                val.next = max_val
+            else:
+                val.next = din
         elif clk:
             val.next = val + 1
             if val.next > max_val:
                 val.next = min_val
-
+                carry_out.next = 1
 
     @always(val)
     def set_output():
@@ -27,11 +32,12 @@ def ring_counter(clk, rst, dout, din, ld, min_val=0, max_val=9):
 def convert_inst(hdl):
     clk = Signal(bool(0))
     rst = Signal(bool(1))
-    dout = Signal(intbv(0, min=0, max=10))
-    din = Signal(intbv(0, min=0, max=10))
+    dout = Signal(intbv(0, min=0, max=16))
+    din = Signal(intbv(0, min=0, max=16))
     ld = Signal(bool(1))
+    carry_out = Signal(bool(0))
 
-    inst = ring_counter(clk, rst, dout, din, ld)
+    inst = ring_counter(clk, rst, dout, din, ld, carry_out)
 
     inst.convert(hdl=hdl, name='ring_counter')
 
@@ -39,7 +45,3 @@ def convert_inst(hdl):
 if __name__ == '__main__':
     convert_inst('Verilog')
     convert_inst('VHDL')
-
-
-
-
--- Labs/Lab 3/clock_myhdl/ring_counter_test.py
+++ Labs/Lab 3/clock_myhdl/ring_counter_test.py
@@ -6,26 +6,29 @@ from ring_counter import ring_counter
 
 class TestRingCounter(unittest.TestCase):
     def test_increment(self):
-        def test(clk, rst, dout, din, ld):
+        def test(clk, rst, dout, din, ld, carry_out, max_val):
             compare = int(dout.val)
             print()
-            print('Clock dOut compare')
-            print(f'{int(clk)} {dout} {compare}')
+            print(f'max_val = {max_val}')
+            print('Clock dOut compare carry_out')
+            print(f'{int(clk)} {dout} {compare} {int(carry_out)}')
             for i in range(30):
                 clk.next = not clk
                 compare = int(dout.val)
                 # Make compare wrap around
-                if compare == 9:
+                if compare == max_val:
                     compare = -1
                 yield delay(10)
-                print(f'{int(clk)} {dout} {compare}')
+                print(f'{int(clk)} {dout} {compare} {int(carry_out)}')
                 if clk:
                     self.assertEqual(compare + 1, dout)
+                    if compare == -1:
+                        self.assertEqual(1, carry_out)
 
         self.run_test(test)
 
     def test_reset(self):
-        def test(clk, rst, dout, din, ld):
+        def test(clk, rst, dout, din, ld, carry_out, max_val):
             print()
             print('Clock dOut reset')
             print(f'{int(clk)} {dout} {rst}')
@@ -34,40 +37,48 @@ class TestRingCounter(unittest.TestCase):
                 if randint(0, 3):
                     rst.next = not rst
                 yield delay(10)
-                print(f'{int(clk)} {dout} {rst}')
+                print(f'{int(clk)} {dout} {int(rst)}')
                 if not rst:
                     self.assertEqual(0, dout)
 
         self.run_test(test)
 
     def test_load(self):
-        def test(clk, rst, dout, din, ld):
+        def test(clk, rst, dout, din, ld, carry_out, max_val):
             rand = randint(0, 9)
+            compare = rand if rand <= max_val else max_val
             print()
-            print('Clock dOut ld rand')
-            print(f'{int(clk)} {dout} {int(ld)} {rand}')
+            print(f'max_val = {max_val}')
+            print('Clock dOut ld rand compare')
+            print(f'{int(clk)} {dout} {int(ld)} {rand} {compare}')
             for i in range(100):
-                rand = randint(0, 9)
+                rand = randint(0, max_val+3)
+                compare = rand if rand <= max_val else max_val
                 din.next = rand
                 clk.next = not clk
                 if not randint(0, 5):
                     ld.next = not ld
                 yield delay(10)
-                print(f'{int(clk)} {dout} {int(ld)} {rand}')
+                print(f'{int(clk)} {dout} {int(ld)} {rand} {compare}')
                 if not ld:
-                    self.assertEqual(rand, dout)
+                    self.assertEqual(compare, dout)
 
         self.run_test(test)
 
     def run_test(self, test):
         clk = Signal(bool(0))
         rst = Signal(bool(1))
-        dout = Signal(intbv(0, min=0, max=10))
-        din = Signal(intbv(0, min=0, max=10))
+        dout = Signal(intbv(0, min=0, max=16))
+        din = Signal(intbv(0, min=0, max=16))
         ld = Signal(bool(1))
+        carry_out = Signal(bool(0))
 
-        inst = ring_counter(clk, rst, dout, din, ld)
-        check = test(clk, rst, dout, din, ld)
+        inst = ring_counter(clk, rst, dout, din, ld, carry_out)
+        check = test(clk, rst, dout, din, ld, carry_out, 9)
+        sim = Simulation(inst, check)
+        sim.run(quiet=1)
+        inst = ring_counter(clk, rst, dout, din, ld, carry_out, max_val=5)
+        check = test(clk, rst, dout, din, ld, carry_out, 5)
         sim = Simulation(inst, check)
         sim.run(quiet=1)
 
