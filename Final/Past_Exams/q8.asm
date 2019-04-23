f194f1e78776501daba68e7577509cd5ab197fd1
Jasper Chan <jasperchan515@gmail.com>
2019-04-22 18:13:32 -0700

    Fix question 8

--- Final/Past_Exams/q8.asm
+++ Final/Past_Exams/q8.asm
@@ -4,12 +4,18 @@ org 000BH
     ljmp isr_T0
 
 isr_T0:
-    ; Carry flag is stored in PSW
-    ; Also, PSW should be popped first to maintain parity
-    ; (Unlikely to be a big deal but no real reason to not)
+    ; Note: stopping the clock while we reset will slightly increase
+    ; the interval between interrupts.
+    ; Whether or not this is acceptable largely depends on the specific
+    ; application and whether the timer reset value takes this into account.
     clr TR0
     mov TH0, #high(1000H)
     mov TL0, #low(1000H)
+    clr TF0
+    ; Starting the timer here will shorten the timer interval
+    ; (i.e. shorten the time that the timer is stopped).
+    ; For timing critical applications this line may have to be moved
+    setb TR0
     push PSW
     push ACC
     mov A, P0
